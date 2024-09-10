f437490927e89066fbf16bea88c93804dcb83203
worthy11 <slotamax15@gmail.com>
2024-09-10 03:24:28 +0200

    URL test

--- hackyeah2024/urls.py
+++ hackyeah2024/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home page')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
