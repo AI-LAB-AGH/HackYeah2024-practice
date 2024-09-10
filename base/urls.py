6f594dddc2cd755adb55b45685794841beaeb5b5
worthy11 <slotamax15@gmail.com>
2024-09-10 05:21:18 +0200

    Fixed variable name

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
-    path('', views.index, name="index"),
+urlpatterns = [
+    path('', views.index, name="home"),
 ]
--- hackyeah2024/settings.py
+++ hackyeah2024/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS =[
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
