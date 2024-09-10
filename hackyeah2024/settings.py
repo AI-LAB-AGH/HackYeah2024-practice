a73795d4811938232d64a7f8f22e06992688850e
worthy11 <slotamax15@gmail.com>
2024-09-10 06:20:00 +0200

    Successful app lauch

--- hackyeah2024/settings.py
+++ hackyeah2024/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/dist/static'
+    BASE_DIR / 'frontend/dist/assets'
 ]
 
 # Default primary key field type
