3c656f0adc92b88ba72378503234f4a14f2f9fe4
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-09-26 16:13:16 +0200

    torch models section

---
+++ Machine_learning/model_torch.py
@@ -0,0 +1,9 @@
+class ModelTorch:
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        "Class to load models from torchvision and torchaudio"
+
+    def get_model(self):
+        pass
