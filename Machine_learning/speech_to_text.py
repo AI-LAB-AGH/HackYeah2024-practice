2fb37cabe36773ebd75cd17337747dd91fc351d8
AdamStajek <adasta146@gmail.com>
2024-09-28 19:16:50 +0200

    messing with paths

--- Machine_learning/speech_to_text.py
+++ Machine_learning/speech_to_text.py
@@ -7,9 +7,10 @@ import json
 class SpeechToText:
     def __init__(self) -> None:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = whisper.load_model("small", device = self.device)
-        self.path = "Machine_learning/wetransfer_hackyeah-2024-breakwordtraps_2024-09-28_0449"
-        self.audio_folder = "Machine_learning/audio_folder"
+        self.model = whisper.load_model("large", device = self.device)
+        self.path = "E:/Python/Projects/HackYeah2024-practice/NLP_work/videos"
+        self.audio_folder = "E:/Python/Projects/HackYeah2024-practice/NLP_work/audio_folder"
+        self.transcripts_path = "E:/Python/Projects/HackYeah2024-practice/NLP_work/transcripts"
 
     def __repr__(self) -> str:
         return "Class to load model from whisper_timestamped"
@@ -27,7 +28,7 @@ class SpeechToText:
             prefix = mp4_path.split(".")[0]
             print(f"Converting {mp4_path} to wav")
             clip = mp.VideoFileClip(f"{self.path}/{mp4_path}")
-            clip.audio.write_audiofile(f"{self.path}/{prefix}.wav")
+            clip.audio.write_audiofile(f"{self.audio_folder}/{prefix}.wav")
             print(f"Converted {mp4_path} to wav")
 
     def transcribe_wav(self) -> str:
@@ -36,7 +37,7 @@ class SpeechToText:
                 if file.endswith(".wav"):
                     prefix = file.split(".")[0]
                     print(f"Transcribing {file}")
-                    audio = whisper.load_audio(f"{self.audio_folder}/{file}")
+                    audio = whisper.load_audio("E:/Python/Projects/HackYeah2024-practice/NLP_work/audio_folder/HY_2024_film_04.wav")
                     result = whisper.transcribe(self.model, audio = audio, language = "pl", vad = "auditok")
 
                     with open(f"{self.path}/{prefix}.json", "w") as f:
