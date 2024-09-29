ccc98e5b0b33a7204c7ed9b39caec1f83aba9a70
lidiamoryc <lidiamoryc@onet.pl>
2024-09-29 09:30:30 +0200

    new frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App3.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Jeśli masz dodatkowy plik CSS
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('app')
 );
